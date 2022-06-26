5be2d7f7480ddcf55bfd8ac7d7ecf6043c3a77ff
christopher-besch <mail@chris-besch.com>
2022-06-26 14:11:28 +0200

    remove cursor when hovering

--- src/modes/circle_mode.ts
+++ src/modes/circle_mode.ts
@@ -3,12 +3,16 @@ import { delete_task, push_task } from "../engine/task_solver";
 
 // bind all required callbacks / event handlers
 export function init_circle_mode(cvd: CompareViewData): void {
+    cvd.canvas.onmouseenter = () => {
+        document.documentElement.style.cursor = "none";
+    };
     cvd.canvas.onmousemove = (e) => {
         // update circle position
         cvd.circle_pos = [e.offsetX, e.offsetY];
         push_task(cvd, Task.update_circle);
     };
     cvd.canvas.onmouseleave = () => {
+        document.documentElement.style.cursor = "default";
         push_task(cvd, Task.remove_circle);
     };
 
