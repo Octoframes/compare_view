4f94af48831040d60007dacaf973381a58a8cfe8
christopher-besch <mail@chris-besch.com>
2022-07-10 09:14:07 +0000

    Deploying to pages from @ Octoframes/compare_view@f8a81dfb6022bcd99c0f34f7bd68f67cc52ca5b1 🚀

---
+++ component_compare_view.js
@@ -0,0 +1,1 @@
+var compare_view;(()=>{"use strict";var e={};(e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})})(e),compare_view=e})();
