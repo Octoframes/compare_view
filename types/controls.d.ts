a04f54bdb93a7d1d1fd5e233a1a68b4fbedaebe2
christopher-besch <mail@chris-besch.com>
2022-07-12 19:25:52 +0000

    Deploying to deploy from @ Octoframes/compare_view@c4a9f5c29bb580dfa69d864e63f1a14b0067f4a5 🚀

---
+++ types/controls.d.ts
@@ -0,0 +1,3 @@
+import { CompareViewData } from "./compare_view_data";
+export declare function reload_checkboxes(cvd: CompareViewData): void;
+export declare function attach_control_events(cvd: CompareViewData): void;
---
+++ types/task_solver.d.ts
@@ -0,0 +1,4 @@
+import { CompareViewData, Task } from "./compare_view_data";
+export declare function delete_task(cvd: CompareViewData, task: Task): void;
+export declare function push_task(cvd: CompareViewData, task: Task): void;
+export declare function contains_task(cvd: CompareViewData, task: Task): boolean;
