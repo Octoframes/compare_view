45a6b37b4c68b1b4621b3bc3d315b322db1fa64b
christopher-besch <mail@chris-besch.com>
2022-07-15 12:35:47 +0000

    Deploying to dist from @ Octoframes/compare_view@2cf1c78d13fb2dfca66e9593b1b94d4568ae813e 🚀

--- types/load_cvd.d.ts
+++ types/load_cvd.d.ts
@@ -1,15 +1,3 @@
-import { Mode, ControlData } from "./compare_view_data";
-export interface Config {
-    start_mode?: Mode;
-    circumference_fraction?: number;
-    circle_size?: number;
-    circle_fraction?: number;
-    show_circle?: boolean;
-    revolve_imgs_on_click?: boolean;
-    slider_fraction?: number;
-    slider_time?: number;
-    rate_function?: (x: number) => number;
-    start_slider_pos?: number;
-    show_slider?: boolean;
-}
+import { ControlData } from "./compare_view_data";
+import { Config } from "./cfg";
 export declare function load_cvd(image_urls: string[], ctx: CanvasRenderingContext2D, cfg: Config, ctrl_data?: ControlData): void;
