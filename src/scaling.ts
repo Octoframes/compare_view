6846d5d74416f0ec0a80fa98ebf7d2db51fe4796
christopher-besch <mail@chris-besch.com>
2022-07-15 16:06:00 +0200

    undo scaling

--- src/scaling.ts
+++ src/scaling.ts
@@ -1,19 +1,5 @@
 import { choose_cfg, Config } from "./cfg";
 import { CompareViewData, Task } from "./compare_view_data";
-import { push_task } from "./task_solver";
-
-// make compare_view instance behave like image without scaling improperly -> needs to get called every time viewport size changes
-function reload_canvas_resolution(cvd: CompareViewData, img_resolution: [number, number]): void {
-    // make canvas as big as images
-    cvd.canvas.width = img_resolution[0];
-    cvd.canvas.height = img_resolution[1];
-    // scale canvas back to perfectly fill space
-    let rect = cvd.canvas.getBoundingClientRect();
-    cvd.canvas.width = rect.width;
-    cvd.canvas.height = rect.height;
-    cvd.width = rect.width;
-    cvd.height = rect.height;
-}
 
 // needs to get called every time canvas size changes
 function load_fractions(cvd: CompareViewData, cfg: Config): void {
@@ -25,11 +11,10 @@ function load_fractions(cvd: CompareViewData, cfg: Config): void {
 }
 
 export function load_canvas_scaling(cvd: CompareViewData, img_resolution: [number, number], cfg: Config): void {
-    window.addEventListener("resize", () => {
-        reload_canvas_resolution(cvd, img_resolution);
-        load_fractions(cvd, cfg);
-        push_task(cvd, Task.none);
-    });
-    reload_canvas_resolution(cvd, img_resolution);
+    cvd.canvas.width = img_resolution[0];
+    cvd.canvas.height = img_resolution[1];
+    cvd.width = cvd.canvas.width;
+    cvd.height = cvd.canvas.height;
+
     load_fractions(cvd, cfg);
 }
