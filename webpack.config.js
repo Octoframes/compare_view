85d6e211d28fd70f8465da21df9b8b33bfc51bf3
christopher-besch <mail@chris-besch.com>
2022-07-17 13:13:31 +0200

    rollback

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,81 @@
+// const path = require("path");
+// const { experiments } = require("webpack");
+
+// module.exports = (env) => {
+//     return {
+//         // can be development or production
+//         mode: env["production"] ? "production" : "development",
+//         // eval good for development
+//         devtool: env["production"] ? false : "eval-source-map",
+//         // only entry file, include any imported files
+//         entry: {
+//             // when directly importing from browser without bundler
+//             browser_compare_view: {
+//                 import: "./src/browser/compare_view.ts",
+//                 // allow browser to access exposed functions
+//                 // library: {
+//                 //     name: "compare_view",
+//                 //     type: "var",
+//                 // },
+//             },
+//             // when using react components and bundler
+//             // component_compare_view: {
+//             //     import: "./src/component/compare_view.tsx",
+//             //     library: {
+//             //         // type: "commonjs-static",
+//             //         type: "module",
+//             //     },
+//             // },
+//             // example_react_index: {
+//             //     import: "./src/component/example/react_index.tsx",
+//             // },
+//         },
+//         module: {
+//             rules: [
+//                 {
+//                     // when test passed
+//                     test: /\.tsx?$/,
+//                     // use ts-loader to compile
+//                     use: "ts-loader",
+//                     include: [path.resolve(__dirname, "src")],
+//                 },
+//             ],
+//         },
+//         resolve: {
+//             extensions: [".ts", ".js", ".tsx", ".jsx"],
+//         },
+//         output: {
+//             // tell dev server where to serve code in memory from
+//             publicPath: "public/dist",
+//             // template based on keys in entry
+//             filename: "[name].js",
+//             // need absolute outputpath
+//             path: path.resolve(__dirname, "public/dist"),
+//             library: {
+//                 name: "compare_view",
+//                 type: "var",
+//             },
+//         },
+//         devServer: {
+//             static: {
+//                 // load uncompiled files (e.g. html files) from public dir
+//                 directory: path.resolve(__dirname, "public"),
+//                 // where to serve them from
+//                 publicPath: "/",
+//             },
+//             devMiddleware: {
+//                 // where to serve compiled files from
+//                 publicPath: "/dist",
+//             },
+//             hot: true,
+//         },
+//         experiments: {
+//             outputModule: true,
+//         },
+//     };
+// };
+
 const path = require("path");
-const { experiments } = require("webpack");
 
 module.exports = (env) => {
     return {
@@ -9,26 +85,9 @@ module.exports = (env) => {
         devtool: env["production"] ? false : "eval-source-map",
         // only entry file, include any imported files
         entry: {
-            // when directly importing from browser without bundler
-            browser_compare_view: {
-                import: "./src/browser/compare_view.ts",
-                // allow browser to access exposed functions
-                library: {
-                    name: "compare_view",
-                    type: "var",
-                },
-            },
-            // when using react components and bundler
-            component_compare_view: {
-                import: "./src/component/compare_view.tsx",
-                library: {
-                    // type: "commonjs-static",
-                    type: "module",
-                },
-            },
-            example_react_index: {
-                import: "./src/component/example/react_index.tsx",
-            },
+            browser_compare_view: "./src/browser/compare_view.ts",
+            component_compare_view: "./src/component/compare_view.tsx",
+            example_react_index: "./src/component/example/react_index.tsx",
         },
         module: {
             rules: [
@@ -51,6 +110,11 @@ module.exports = (env) => {
             filename: "[name].js",
             // need absolute outputpath
             path: path.resolve(__dirname, "public/dist"),
+            // allow browser to access exposed functions
+            library: {
+                name: "compare_view",
+                type: "var",
+            },
         },
         devServer: {
             static: {
@@ -64,9 +128,6 @@ module.exports = (env) => {
                 publicPath: "/dist",
             },
             hot: true,
-        },
-        experiments: {
-            outputModule: true,
         },
     };
 };
