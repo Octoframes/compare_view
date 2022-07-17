4769a3d625491e02625abe88786c9b616238ffd2
christopher-besch <mail@chris-besch.com>
2022-07-17 08:35:04 +0000

    Deploying to dist from @ Octoframes/compare_view@c52a8a94d383a0623129a0ce97be6a621e13d62c 🚀

--- types/component/compare_view.d.ts
+++ types/component/compare_view.d.ts
@@ -3,9 +3,9 @@ import React from "react";
 interface ComponentConfig extends Config {
     create_controls?: boolean;
 }
-export interface CompareViewProps {
+interface CompareViewProps {
     image_urls: string[];
     config?: ComponentConfig;
 }
-export declare const CompareView: React.FC<CompareViewProps>;
-export {};
+declare const CompareView: React.FC<CompareViewProps>;
+export default CompareView;
