cade0834ab0f4708da78787640d6c0270cf57baa
christopher-besch <mail@chris-besch.com>
2022-08-01 14:18:13 +0000

    Deploying to dist from @ Octoframes/compare_view@17ac4ec226181f4238ce4ee4d330fba440fe84dc 🚀

--- types/scaling.d.ts
+++ types/scaling.d.ts
@@ -1,3 +1,2 @@
-import { Config } from "./cfg";
 import { CompareViewData } from "./compare_view_data";
-export declare function load_canvas_scaling(cvd: CompareViewData, img_resolution: [number, number], cfg: Config): void;
+export declare function load_canvas_scaling(cvd: CompareViewData, img_resolution: [number, number]): void;
