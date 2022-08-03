c52d11fdf53a60ddcb82b2d84faafe05389bf34b
christopher-besch <mail@chris-besch.com>
2022-08-03 19:22:23 +0000

    Deploying to dist from @ Octoframes/compare_view@dc72dfc6302f0fe92d4f5c2ab7304c632915eef5 🚀

---
+++ types/drag_drop/drag_drop.d.ts
@@ -0,0 +1,2 @@
+export declare function make_draggable(drag_target: HTMLDivElement): void;
+export declare function load(drag_target_class: string, default_image_url: string, on_new_imgs: (image_urls: string[]) => void): void;
